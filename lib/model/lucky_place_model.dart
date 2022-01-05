97f9c400c5da8a4de10aefa6f14199afacd984a5
Abir Ahsan <abirahsan122@gmail.com>
2022-01-05 20:01:35 +0600

    change title text

--- lib/model/lucky_place_model.dart
+++ lib/model/lucky_place_model.dart
@@ -7,18 +7,18 @@ class LuckyPlace {
   LuckyPlace({this.tiTle, this.placeId, this.description, this.dateTime});
 
   LuckyPlace.fromJson(Map<String, dynamic> json) {
-    tiTle = json['ti\$tle' ''].toString();
+    tiTle = json['ti\$tle'].toString();
     placeId = json['placeId'].toString();
     description = json['description'].toString();
     dateTime = json['dateTime'].toString();
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['ti\$tle'] = this.tiTle;
-    data['placeId'] = this.placeId;
-    data['description'] = this.description;
-    data['dateTime'] = this.dateTime;
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['ti\$tle'] = tiTle;
+    data['placeId'] = placeId;
+    data['description'] = description;
+    data['dateTime'] = dateTime;
     return data;
   }
 }
